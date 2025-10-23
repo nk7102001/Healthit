baf0e64e033acdb8d3fc01a683c46f81e70c5266
nk7102001 <165237480+nk7102001@users.noreply.github.com>
2025-10-23 19:41:24 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({
 
 }, { timestamps: true });
 
-// ✅ Hash password before saving
+
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
 
